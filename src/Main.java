755acf83ddb054ff7912bff793408118417d0f52
shmll <shmllslmt@gmail.com>
2023-07-26 10:05:07 +0800

    Read Data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,83 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File kpmgenset = new File("kpmgenset.csv");
+
+        int index = 0;
+
+        ArrayList<SolarData> solarData = new ArrayList<>();
+        ArrayList<String> states = new ArrayList<>();
+        ArrayList<String> levels = new ArrayList<>();
+        ArrayList<String> categories = new ArrayList<>();
+        ArrayList<String> locations = new ArrayList<>();
+
+        if(kpmgenset.exists()) {
+            System.out.println("File exists");
+
+            if(kpmgenset.canRead()) {
+                System.out.println("File is readable");
+            } else {
+                System.out.println("File is unreadable");
+            }
+
+            try(Scanner reader = new Scanner(kpmgenset)) {
+                while (reader.hasNext()) {
+                    String line = reader.nextLine();
+                    if(index > 0) {
+                        String[] items = line.split(",");
+
+                        SolarData data = new SolarData(items[0], items[1], items[2], items[3], items[4], items[5], items[6], items[7], items[8], items[9]);
+
+                        solarData.add(data);
+
+                        System.out.print(index + " ");
+                        System.out.println(data);
+                        System.out.println();
+                    }
+                    index++;
+                }
+
+                for(SolarData row: solarData) {
+                    if(!(states.contains(row.state))) {
+                        states.add(row.state);
+                    }
+                    if(!(levels.contains(row.level))) {
+                        levels.add(row.level);
+                    }
+                    if(!(categories.contains(row.category))) {
+                        categories.add(row.category);
+                    }
+                    if(!(locations.contains(row.location))) {
+                        locations.add(row.location);
+                    }
+                }
+
+                System.out.println("Number of Unique States: "+states.size());
+                for(String state: states){
+                    System.out.println(state);
+                }
+                System.out.println("Number of Unique Levels: "+levels.size());
+                for(String level: levels) {
+                    System.out.println(level);
+                }
+                System.out.println("Number of Unique Locations: "+locations.size());
+                for(String location: locations) {
+                    System.out.println(location);
+                }
+                System.out.println("Number of Unique Categories: "+categories.size());
+                for(String category: categories) {
+                    System.out.println(category);
+                }
+
+            } catch (FileNotFoundException exception) {
+                System.out.println(exception.getMessage());
+            }
+        } else {
+            System.out.println("File does not exists");
+        }
     }
 }
--- src/SolarData.java
+++ src/SolarData.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class SolarData {
+public class SolarData {
+    public String state;
+    public String deo;
+    public String level;
+    public String category;
+    public String schoolType;
+    public String location;
+    public String inland;
+    public String schoolCode;
+    public String schoolName;
+    public String source;
+    // Parameterized Constructor
+    public SolarData(String state,String deo,String level,String category,String schoolType,String location,String inland,String schoolCode,String schoolName,String source) {
+        this.state = state;
+        this.deo = deo;
+        this.level = level;
+        this.category = category;
+        this.schoolType = schoolType;
+        this.location = location;
+        this.inland = inland;
+        this.schoolCode = schoolCode;
+        this.schoolName = schoolName;
+        this.source = source;
+    }
+
+    @Override
+    public String toString() {
+        return "State = "+state+", DOE = "+deo+", Level = "+level+", Category = "+category+", School Type = "+schoolType+", Location = "+location+", Inland = "+inland+", School Code = "+schoolCode+", School Name = "+schoolName+", Electric Source = "+source;
+    }
 }
